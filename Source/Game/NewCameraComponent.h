47355887bb6f4421fd5dc926ab786c6eb132d7fc
WhalesAreDelicious <weilony@hotmail.com>
2016-11-13 12:36:18 +1100

    Added first iteration of camera movement Game now uses Unreal Engine 4.13

---
+++ Source/Game/NewCameraComponent.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Camera/CameraComponent.h"
+#include "NewCameraComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAME_API UNewCameraComponent : public UCameraComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
